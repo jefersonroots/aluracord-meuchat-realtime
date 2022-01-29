24eeff7e0a8d435e0a0efa5a663e45015b7ca18e
Jeferson S <64113091+jefersonroots@users.noreply.github.com>
2022-01-29 20:52:21 -0300

    Initial commit

---
+++ pages/chat.js
@@ -0,0 +1,216 @@
+import { Box, Text, TextField, Image, Button } from "@skynexui/components";
+import React from "react";
+import appConfig from "../config.json";
+
+export default function ChatPage() {
+  const [mensagem, setMensagem] = React.useState("");
+  const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+
+  /*
+    // Usuário
+    - Usuário digita no campo textarea
+    - Aperta enter para enviar
+    - Tem que adicionar o texto na listagem
+    
+    // Dev
+    - [X] Campo criado
+    - [X] Vamos usar o onChange usa o useState (ter if pra caso seja enter pra limpar a variavel)
+    - [X] Lista de mensagens 
+    */
+  function handleNovaMensagem(novaMensagem) {
+    const mensagem = {
+      id: listaDeMensagens.length + 1,
+      de: "JefersonRoots",
+      texto: novaMensagem,
+      from: TestFetch()
+    };
+
+    setListaDeMensagens([mensagem, ...listaDeMensagens]);
+    setMensagem("");
+  }
+
+  return (
+    <Box
+      styleSheet={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: appConfig.theme.colors.primary[500],
+        backgroundImage:
+          "url(https://lfrfu3w8vyl1p3bai347o9xv-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Frequently-Asked-Questions-on-Blockchain-Answered-by-Expert-Jordan-Lindsey-1280x640.jpeg)",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        backgroundBlendMode: "multiply",
+        color: appConfig.theme.colors.neutrals["000"]
+      }}
+    >
+      <Box
+        styleSheet={{
+          display: "flex",
+          flexDirection: "column",
+          flex: 1,
+          boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+          borderRadius: "5px",
+          backgroundColor: appConfig.theme.colors.neutrals[400],
+          height: "100%",
+          maxWidth: "95%",
+          maxHeight: "95vh",
+          padding: "32px"
+        }}
+      >
+        <Header />
+        <Box
+          styleSheet={{
+            position: "relative",
+            display: "flex",
+            flex: 1,
+            height: "80%",
+            backgroundColor: appConfig.theme.colors.neutrals[500],
+            flexDirection: "column",
+            borderRadius: "5px",
+            padding: "16px"
+          }}
+        >
+          <MessageList mensagens={listaDeMensagens} />
+          {/* {listaDeMensagens.map((mensagemAtual) => {
+                        return (
+                            <li key={mensagemAtual.id}>
+                                {mensagemAtual.de}: {mensagemAtual.texto}
+                            </li>
+                        )
+                    })} */}
+          <Box
+            as="form"
+            styleSheet={{
+              display: "flex",
+              alignItems: "center"
+            }}
+          >
+            <TextField
+              value={mensagem}
+              onChange={(event) => {
+                const valor = event.target.value;
+                setMensagem(valor);
+              }}
+              onKeyPress={(event) => {
+                if (event.key === "Enter") {
+                  event.preventDefault();
+                  handleNovaMensagem(mensagem);
+                }
+              }}
+              placeholder="Insira sua mensagem aqui..."
+              type="textarea"
+              styleSheet={{
+                width: "100%",
+                border: "0",
+                resize: "none",
+                borderRadius: "5px",
+                padding: "6px 8px",
+                backgroundColor: appConfig.theme.colors.neutrals[800],
+                marginRight: "12px",
+                color: appConfig.theme.colors.neutrals[200]
+              }}
+            />
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
+
+function Header() {
+  return (
+    <>
+      <Box
+        styleSheet={{
+          width: "100%",
+          marginBottom: "16px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between"
+        }}
+      >
+        <Text variant="heading5">Chat</Text>
+        <Button
+          variant="tertiary"
+          colorVariant="neutral"
+          label="Logout"
+          href="/"
+        />
+      </Box>
+    </>
+  );
+}
+
+// COMO PEGAR DADOS DE URL/JSON ( CALLBACK )
+function TestFetch() {
+  fetch("https://api.github.com/users/jefersonroots").then(
+    async (respostaDoServidor) => {
+      const respostaEsperada = await respostaDoServidor.json();
+      console.log(respostaEsperada.location); // PEGANDO LOCALIDADE
+    }
+  );
+}
+
+function MessageList(props) {
+  console.log(props);
+  return (
+    <Box
+      tag="ul"
+      styleSheet={{
+        overflow: "scroll",
+        display: "flex",
+        flexDirection: "column-reverse",
+        flex: 1,
+        color: appConfig.theme.colors.neutrals["000"],
+        marginBottom: "16px"
+      }}
+    >
+      {props.mensagens.map((mensagem) => {
+        return (
+          <Text
+            key={mensagem.id}
+            tag="li"
+            styleSheet={{
+              borderRadius: "5px",
+              padding: "6px",
+              marginBottom: "12px",
+              hover: {
+                backgroundColor: appConfig.theme.colors.neutrals[700]
+              }
+            }}
+          >
+            <Box
+              styleSheet={{
+                marginBottom: "8px"
+              }}
+            >
+              <Image
+                styleSheet={{
+                  width: "20px",
+                  height: "20px",
+                  borderRadius: "50%",
+                  display: "inline-block",
+                  marginRight: "8px"
+                }}
+                src={`https://github.com/jefersonroots.png`}
+              />
+              <Text tag="strong">{mensagem.de}</Text>
+              <Text
+                styleSheet={{
+                  fontSize: "10px",
+                  marginLeft: "8px",
+                  color: appConfig.theme.colors.neutrals[300]
+                }}
+                tag="span"
+              >
+                {new Date().toLocaleDateString()}
+              </Text>
+            </Box>
+            {mensagem.texto}
+          </Text>
+        );
+      })}
+    </Box>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,191 @@
+import React from "react";
+import { Box, Button, Text, TextField, Image } from "@skynexui/components";
+import appConfig from "../config.json";
+import { useRouter } from "next/router";
+
+function Titulo(props) {
+  const Tag = props.tag || "h1";
+  return (
+    <>
+      <Tag>{props.children}</Tag>
+      <style jsx>{`
+        ${Tag} {
+          color: orange;
+          font-size: 24px;
+          font-weight: 600;
+        }
+      `}</style>
+    </>
+  );
+}
+
+// // Componente React
+// function HomePage() {
+//   // JSX
+//   return (
+//     <div>
+//       <GlobalStyle />
+//       <Titulo tag="h2"> Boas vindas de volta!!</Titulo>
+//       <h2> Discord - Alura Matrix</h2>
+//       <style jsx>
+//         {`
+//           h2 {
+//             color: ${appConfig.theme.colors.neutrals["200"]};
+//           }
+//         `}
+//       </style>
+//     </div>
+//   );
+// }
+// export default HomePage;
+
+export default function PaginaInicial() {
+  // const username = "JefersonRoots";
+  const [username, setUsername] = React.useState("JefersonRoots");
+  const roteamento = useRouter();
+  return (
+    <>
+      <Box
+        styleSheet={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: appConfig.theme.colors.primary[500],
+          backgroundImage:
+            "url(https://lfrfu3w8vyl1p3bai347o9xv-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/Frequently-Asked-Questions-on-Blockchain-Answered-by-Expert-Jordan-Lindsey-1280x640.jpeg)",
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundBlendMode: "multiply"
+        }}
+      >
+        <Box
+          styleSheet={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            flexDirection: {
+              xs: "column",
+              sm: "row"
+            },
+            width: "100%",
+            maxWidth: "700px",
+            borderRadius: "5px",
+            padding: "32px",
+            margin: "16px",
+            boxShadow: "0 2px 10px 0 rgb(0 0 0 / 20%)",
+            backgroundColor: appConfig.theme.colors.neutrals[700]
+          }}
+        >
+          {/* Formulário */}
+          <Box
+            as="form"
+            onSubmit={function (infEvent) {
+              infEvent.preventDefault();
+              console.log("Submetido o form.");
+              roteamento.push("/chat");
+            }}
+            styleSheet={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              width: { xs: "100%", sm: "50%" },
+              textAlign: "center",
+              marginBottom: "32px"
+            }}
+          >
+            <Titulo tag="h2">BEM VINDO!</Titulo>
+            <Text
+              variant="body3"
+              styleSheet={{
+                marginBottom: "32px",
+                color: appConfig.theme.colors.neutrals[300]
+              }}
+            >
+              {appConfig.name}
+            </Text>
+
+            <TextField
+              value={username}
+              onChange={function (event) {
+                console.log("digitou:", event.target.value);
+                //onde esta o valor?
+                // const contDigitacao = event.target.value;
+                // const contador = 1;
+                const valor = event.target.value;
+                //trocando os valores de variavei através do react e avisando quem precisa
+                setUsername(valor);
+
+                // if (contDigitacao < 2) {
+                //   console.log("Menos de 2 ", contDigitacao.width);
+                // } else {
+                //   console.log("contando", event.target.value);
+                // }
+              }}
+              fullWidth
+              textFieldColors={{
+                neutral: {
+                  textColor: appConfig.theme.colors.neutrals[200],
+                  mainColor: appConfig.theme.colors.neutrals[900],
+                  mainColorHighlight: appConfig.theme.colors.primary[500],
+                  backgroundColor: appConfig.theme.colors.neutrals[200],
+                  fontWeight: "bold"
+                }
+              }}
+            />
+            <Button
+              type="submit"
+              label="Entrar"
+              fullWidth
+              buttonColors={{
+                contrastColor: appConfig.theme.colors.neutrals["E3F9E5"],
+                mainColor: appConfig.theme.colors.primary[500],
+                mainColorLight: appConfig.theme.colors.primary[400],
+                mainColorStrong: appConfig.theme.colors.primary[600],
+                fontWeight: "bold"
+              }}
+            />
+          </Box>
+          {/* Formulário */}
+
+          {/* Photo Area */}
+          <Box
+            styleSheet={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              maxWidth: "200px",
+              padding: "16px",
+              backgroundColor: appConfig.theme.colors.neutrals[800],
+              border: "1px solid",
+              borderColor: appConfig.theme.colors.neutrals[999],
+              borderRadius: "10px",
+              flex: 1,
+              minHeight: "240px"
+            }}
+          >
+            <Image
+              styleSheet={{
+                borderRadius: "50%",
+                marginBottom: "16px"
+              }}
+              src={`https://github.com/${username}.png`}
+            />
+            <Text
+              variant="body4"
+              styleSheet={{
+                color: appConfig.theme.colors.neutrals[200],
+                backgroundColor: appConfig.theme.colors.neutrals[900],
+                padding: "3px 10px",
+                borderRadius: "1000px"
+              }}
+            >
+              {username}
+            </Text>
+          </Box>
+          {/* Photo Area */}
+        </Box>
+      </Box>
+    </>
+  );
+}
